8ee43f02fd9ff95de6e14bdb5f89c6bca35f228b
Raihan Mufadhal <raihan23rm@gmail.com>
2025-10-29 13:35:02 +0700

    fix: layout and responsiveness in auth_pages

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -13,22 +13,21 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className='bg-[#0B1FD1] w-full h-screen'>
-
-      <div className='flex flex-row gap-4 p-4 justify-evenly items-center'>
-        <div className='flex flex-col gap-4 justify-between items-center'>
-          <h2 className='font-extrabold text-white text-4xl'>Studyfirst</h2>
-         <span className='font-bold text-white text-2xl w-96'>Langganan untuk akses beragam konten dan fitur belajar menarik</span>
-         <div className='mt-20'>
-           <img src="/primary-form-illustration.png" alt="" className='w-[780px] h-auto'/>
+    <div className='bg-[#0B1FD1] w-full min-h-screen flex items-center justify-center overflow-x-hidden'>
+      <div className='flex flex-col lg:flex-row gap-10 justify-center items-center w-full max-w-7xl'>
+        <div className='flex flex-col gap-4 justify-center items-center text-center lg-text-left lg:items-start'>
+          <h2 className='font-extrabold text-white text-3xl lg:text-4xl self-center'>Studyfirst</h2>
+         <span className='font-bold text-white text-lg lg:text-2xl max-w-md self-center'>Langganan untuk akses beragam konten dan fitur belajar menarik</span>
+         <div className='mt-10'>
+           <img src="/primary-form-illustration.png" alt="" className='w-[300px] sm:w-[400px] lg:w-[600px] h-auto'/>
          </div>
         </div>
          
 
-         <div className=''>
-          <Card className='w-[450px] h-auto'>
+         <div className='w-full sm:w-[400px]'>
+          <Card className='w-full shadow-xl'>
             <CardHeader>
-              <CardTitle className='text-center text-2xl'>Yuk, daftar atau masuk untuk melanjutkan!</CardTitle>
+              <CardTitle className='text-center text-xl lg:text-2xl'>Yuk, daftar atau masuk untuk melanjutkan!</CardTitle>
             </CardHeader>
             <CardContent className='flex flex-col gap-4'>
               <Label>E-Mail</Label>
@@ -36,19 +35,19 @@ const Login = () => {
               <Label>Password</Label>
               <Input placeholder='******'/>
               <Button className='bg-[#0B9FD1] text-white font-bold rounded-full'>Lanjutkan</Button>
-                <CardDescription className='flex flex-row gap-2 items-center'>
-                  <div> ______________________________ </div>
-                   <span className='flex items-center mt-3'>atau</span>
-                  <div> ______________________________ </div>
+                <CardDescription className='flex items-center my-2'>
+                  <div className="grow border-t border-gray-300"></div>
+                    <span className="mx-2 text-gray-500 text-sm">atau</span>
+                  <div className="grow border-t border-gray-300"></div>
                 </CardDescription>
                <Button variant={"outline"} className='flex flex-row gap-2 items-center'>
                 <img src="/google.jpg" alt="" className='w-8 h-auto'/>
                 <h4>Google</h4>
               </Button>
             </CardContent>
-            <CardFooter>
-              <div className="flex flex-row gap-2">
-                <h3>Anda Belum Punya Akun ?</h3>
+            <CardFooter className="flex justify-center">
+              <div className="flex flex-row gap-2 text-sm">
+                <h3>Anda Belum Punya Akun?</h3>
                 <Link to={"/auth/register"} className="text-[#0B9FD1] italic font-bold hover:underline">Register</Link>
               </div>
             </CardFooter>
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -13,25 +13,24 @@ import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div className='bg-[#0B1FD1] w-full h-screen'>
-
-      <div className='flex flex-row gap-4 p-4 justify-evenly items-center'>
-        <div className='flex flex-col gap-4 justify-between items-center'>
-          <h2 className='font-extrabold text-white text-4xl'>Studyfirst</h2>
-         <span className='font-bold text-white text-2xl w-96'>Langganan untuk akses beragam konten dan fitur belajar menarik</span>
-         <div className='mt-20'>
-           <img src="/primary-form-illustration.png" alt="" className='w-[780px] h-auto'/>
+    <div className='bg-[#0B1FD1] w-full min-h-screen flex flex-col justify-center items-center p-6'>
+      <div className='flex flex-col lg:flex-row gap-10 justify-center items-center w-full max-w-7xl'>
+        <div className='flex flex-col gap-4 justify-center items-center text-center lg-text-left lg:items-start'>
+          <h2 className='font-extrabold text-white text-3xl lg:text-4xl self-center'>Studyfirst</h2>
+         <span className='font-bold text-white text-lg lg:text-2xl max-w-md self-center'>Langganan untuk akses beragam konten dan fitur belajar menarik</span>
+         <div className='mt-10'>
+           <img src="/primary-form-illustration.png" alt="" className='w-[300px] sm:w-[400px] lg:w-[600px] h-auto'/>
          </div>
         </div>
          
 
-         <div className=''>
-          <Card className='w-[450px] h-auto'>
+         <div className='w-full sm:w-[400px]'>
+          <Card className='w-full shadow-xl'>
             <CardHeader>
               <CardTitle className='text-center text-2xl'>Yuk, daftar atau masuk untuk melanjutkan!</CardTitle>
             </CardHeader>
             <CardContent className='flex flex-col gap-4'>
-                <Label>Username</Label>
+              <Label>Username</Label>
               <Input placeholder='JhonDoe'/>
               <Label>E-Mail</Label>
               <Input placeholder='example@gmail.com'/>
@@ -39,19 +38,19 @@ const Register = () => {
               <Input placeholder='******'/>
               <Label>Confirm Password</Label>
               <Input placeholder='******'/>
-              <Button className='bg-[#0B9FD1] text-white font-bold rounded-full'>Lanjutkan</Button>
-                <CardDescription className='flex flex-row gap-2 items-center'>
-                  <div> ______________________________ </div>
-                   <span className='flex items-center mt-3'>atau</span>
-                  <div> ______________________________ </div>
+              <Button className='bg-[#0B9FD1] text-white font-bold rounded-full mt-2'>Lanjutkan</Button>
+                <CardDescription className='flex items-center my-2'>
+                  <div className="grow border-t border-gray-300"></div>
+                    <span className="mx-2 text-gray-500 text-sm">atau</span>
+                  <div className="grow border-t border-gray-300"></div>
                 </CardDescription>
-               <Button variant={"outline"} className='flex flex-row gap-2 items-center'>
-                <img src="/google.jpg" alt="" className='w-8 h-auto'/>
+               <Button variant={"outline"} className='flex flex-row gap-2 items-center justify-center'>
+                <img src="/google.jpg" alt="" className='w-6 h-auto'/>
                 <h4>Google</h4>
               </Button>
             </CardContent>
-             <CardFooter>
-              <div className="flex flex-row gap-2">
+             < CardFooter className="justify-center">
+              <div className="flex flex-row gap-2 text-sm">
                 <h3>Anda sudah punya akun ?</h3>
                 <Link to={"/auth/login"} className="text-[#0B9FD1] italic font-bold hover:underline">Login</Link>
               </div>
