e14eb23b02e60ff596169ef5ebcab373629f4ad5
Raihan Mufadhal <raihan23rm@gmail.com>
2025-11-11 19:11:12 +0700

    feat: Add Banner Dashboard

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useIsMobile } from "@/hooks/use-mobile";
 import ChartAttempt from "@/components/fragments/dashboard/chart/chart";
+import TopBanner from "@/components/fragments/dashboard/banner/banner";
 import {
   Card,
   CardContent,
@@ -21,12 +22,12 @@ export default function Dashboard() {
   }, [session]);
   const isMobile = useIsMobile();
   const navigate = useNavigate();
-  if (isPending) {
-    return <div>Loading</div>;
-  }
-  if (error || !session) {
-    navigate("/auth/login");
-  }
+  // if (isPending) {
+  //   return <div>Loading</div>;
+  // }
+  // if (error || !session) {
+  //   navigate("/auth/login");
+  // }
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -34,6 +35,7 @@ export default function Dashboard() {
         {isMobile && <SidebarTrigger />}
         <div className="size-full min-h-screen p-2 max-md:border-l max-md:border-l-gray-400 flex justify-center items-start">
           <div className="w-[95%] bg-white px-2 flex flex-col">
+            <TopBanner/>
             {/* Main Chart Content */}
             <Card className="bg-teal-200">
               <CardContent className="flex max-md:flex-col gap-2">
